7ba4d02dfbd9a3174bc80657b7f1786807d29646
yolafvck <yolafvck@gmail.com>
2021-12-28 23:19:05 +0300

    init commit

---
+++ create_infrastructure.sh
@@ -0,0 +1,12 @@
+printf '%s %s\n' "Start deployment in" "$(date)"
+printf '%s\n' "Cloning infrastructure files from repo"
+git clone https://github.com/yolafvck/infrastructure.git
+cd infrastructure 
+printf '%s\n' "Cloning development files from repo"
+git clone https://github.com/yolafvck/yolafvck.git /home/hal1kov/html/reg
+printf '%s %s\n' "Start building custom php-fpm image in" "$(date)"
+docker build -t "php:7.4-fpm-mysql" - < ./Dockerfile
+printf '%s %s\n' "Start containers in" "$(date)"
+docker-compose up -d
+printf '%s %s\n' "Infrastructure ready in" "$(date)"
+
